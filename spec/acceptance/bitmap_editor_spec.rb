2e82a5dcb2cb8277b2978dc8893fc94816a6509d
William Fish <william.michael.fish@gmail.com>
2019-06-18 20:52:06 +0100

    Adds acceptance spec and all's passing \0/

---
+++ spec/acceptance/bitmap_editor_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+RSpec.describe BitmapEditor do
+  subject(:bitmap_editor) { described_class.new }
+
+  let(:file) { fixture_path("commands_provided.txt") }
+
+  it "returns the correct output" do
+    expected = <<~EXPECTED
+      OOAOO
+      OOZZZ
+      OWOOO
+      OWOOO
+      OWOOO
+      OWOOO
+    EXPECTED
+
+    expect { bitmap_editor.run(file) }.to output(expected).to_stdout
+  end
+end
